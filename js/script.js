a82808316742fb914a1aa1e744cc8c5c7e2196c2
tm-0708 <182177078+tomo0778@users.noreply.github.com>
2025-02-16 13:19:30 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,16 +15,4 @@ $(function () {
         return false;
     });
 
-    $(".game-item .content").hide();
-
-    // クリックで表示/非表示を切り替え
-    $(".toggle-btn").on("click", function () {
-        var content = $(this).closest(".game-item").find(".content"); 
-
-        content.stop().slideToggle(300); // スライドで表示/非表示を切り替え
-
-        // 矢印の変更（→ を ↓ にする）
-        var icon = $(this).text().trim();
-        $(this).text(icon === "→" ? "↓" : "→");
-    });
 });
