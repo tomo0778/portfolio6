d718fd52f2b07a703d4c424529779ca31c515b4a
tm-0708 <182177078+tomo0778@users.noreply.github.com>
2025-02-16 12:45:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 $(function () {
-    //ページ内スクロール
+    // ページ内スクロール
     var navHeight = $(".header").outerHeight();
     $('a[href^="#"]').on("click", function () {
-      var href = $(this).attr("href");
-      var target = $(href == "#" || href == "" ? "html" : href);
-      var position = target.offset().top - navHeight;
-      $("html, body").animate({ scrollTop: position, }, 300, "swing");
-      return false;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? "html" : href);
+        var position = target.offset().top - navHeight;
+        $("html, body").animate({ scrollTop: position }, 300, "swing");
+        return false;
     });
-    //ページトップ
+
+    // ページトップボタン
     $("#js-page-top").on("click", function () {
-      $("body,html").animate({ scrollTop: 0, }, 300);
-      return false;
+        $("body,html").animate({ scrollTop: 0 }, 300);
+        return false;
     });
-  });
+
+    $(".game-item .content").hide();
+
+    // クリックで表示/非表示を切り替え
+    $(".toggle-btn").on("click", function () {
+        var content = $(this).closest(".game-item").find(".content"); 
+
+        content.stop().slideToggle(300); // スライドで表示/非表示を切り替え
+
+        // 矢印の変更（→ を ↓ にする）
+        var icon = $(this).text().trim();
+        $(this).text(icon === "→" ? "↓" : "→");
+    });
+});
