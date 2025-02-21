1037ede4c0fbde97f6cd8bacc84c4a82ada73937
tm-0708 <182177078+tomo0778@users.noreply.github.com>
2025-02-21 09:47:01 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,17 +14,4 @@ $(function () {
         $("body,html").animate({ scrollTop: 0 }, 300);
         return false;
     });
-    
-    $(".game-item .content").hide();
-
-    // クリックで表示/非表示を切り替え
-    $(".toggle-btn").on("click", function () {
-        var content = $(this).closest(".game-item").find(".content"); 
-
-        content.stop().slideToggle(300); // スライドで表示/非表示を切り替え
-
-        // 矢印の変更（→ を ↓ にする）
-        var icon = $(this).text().trim();
-        $(this).text(icon === "→" ? "↓" : "→");
-    });
 });
